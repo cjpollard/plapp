5cf00e0bb80563bb07949fe3b2ed0b461b64cbc9
Colin Pollard <colin@penrillian.com>
2014-03-18 16:47:38 +0000

    cp: initial commit with failing testPreconditions

---
+++ PlappTest/src/com/pdr/plapp/test/MainActivityTest.java
@@ -0,0 +1,32 @@
+package com.pdr.plapp.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+import com.pdr.plapp.MainActivity;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+	
+	private MainActivity mActivity;
+	private TextView mDefaultString;	
+	
+	public MainActivityTest() {
+		super(MainActivity.class);
+	}
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+		setActivityInitialTouchMode(false);
+		
+		mActivity = getActivity();
+		
+		mDefaultString = (TextView) mActivity.findViewById(com.pdr.plapp.R.id.section_label);
+	}
+	
+	public void testPreconditions() {
+		String actualDefaultString = (String) mDefaultString.getText();
+		assertEquals(actualDefaultString, "");
+	}
+
+}
